45e30e07d8164fff6716d142007fe0e710edbe08
dubeypratiksha <piyapratiksha@gmail.com>
2026-03-25 21:43:03 +0530

    Implement boat rescue algorithm in NumberOfBoats.java

---
+++ NumberOfBoats.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        int boats = 0;
+        Arrays.sort(people);
+        int i =0;
+        int j=people.length-1;
+        
+        while(i<=j){
+
+        if(people[i]+people[j]<=limit){
+            j--;
+            i++;
+            boats++;
+        }
+
+        else if(people[i]+people[j]>limit){
+        j--;
+        boats++;
+        }
+    }
+      return boats;  
+    }
+}
